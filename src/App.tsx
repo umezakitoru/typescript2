fd2ad0926a1d011eda856b615cec0ce5c9ed69b6
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-02-06 16:20:44 +0900

    111

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import axios from "axios";
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
+      console.log(res);
+    });
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickFetchData}>データ取得</button>
+    </div>
+  );
 }
