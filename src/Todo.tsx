3367d9ef889b2a66027624f3c732be0bfabbaf91
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-02-11 08:58:13 +0900

    111

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,10 +1,9 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-};
+import { TodoType } from "./types/todo";
 
-export const Todo = (props: TodoType) => {
+export const Todo = (
+  //props: Pick<TodoType, "userId" | "title" | "completed">
+  props: Omit<TodoType, "id">
+) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>;
