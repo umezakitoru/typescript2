1ad5a1d500e757f00f1853b8370eb9a4cee10241
umezakitoru <64876170+umezakitoru@users.noreply.github.com>
2022-02-13 10:14:16 +0900

    111

---
+++ src/UserProfile.tsx
@@ -0,0 +1,18 @@
+import { User } from "./types/user";
+import { VFC } from "react";
+
+type Props = {
+  user: User;
+};
+
+export const UserProfile: VFC<Props> = (props) => {
+  const { user } = props;
+  return (
+    <dl>
+      <dt>名前</dt>
+      <dd>{user.name}</dd>
+      <dt>趣味</dt>
+      <dd>{user.hobbies?.join(" / ")}</dd>
+    </dl>
+  );
+};
